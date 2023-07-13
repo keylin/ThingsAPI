ac67cfcb56727d74efc19cb0b33a4436120d6d6b
linlin <cailinlin389@hellobike.com>
2023-07-14 00:05:29 +0800

    update

--- src/main/java/tx/network/mqtt/ssl/Client.java
+++ src/main/java/tx/network/mqtt/ssl/Client.java
@@ -9,39 +9,39 @@ import io.vertx.mqtt.MqttClientOptions;
 
 public class Client extends AbstractVerticle {
 
-  private static final String MQTT_TOPIC = "/my_topic";
-  private static final String MQTT_MESSAGE = "Hello Vert.x MQTT Client";
-  private static final String BROKER_HOST = "localhost";
-  private static final int BROKER_PORT = 8883;
+    private static final String MQTT_TOPIC = "/my_topic";
+    private static final String MQTT_MESSAGE = "Hello Vert.x MQTT Client";
+    private static final String BROKER_HOST = "localhost";
+    private static final int BROKER_PORT = 8883;
 
-  public static void main(String[] args) {
-    Launcher.executeCommand("run", Client.class.getName());
-  }
+    public static void main(String[] args) {
+        Launcher.executeCommand("run", Client.class.getName());
+    }
 
-  @Override
-  public void start() throws Exception {
-    MqttClientOptions options = new MqttClientOptions();
-      options.setSsl(true);
-      options.setTrustAll(true);
+    @Override
+    public void start() throws Exception {
+        MqttClientOptions options = new MqttClientOptions();
+        options.setSsl(true);
+        options.setTrustAll(true);
 
-    MqttClient mqttClient = MqttClient.create(vertx, options);
+        MqttClient mqttClient = MqttClient.create(vertx, options);
 
-    mqttClient.connect(BROKER_PORT, BROKER_HOST, ch -> {
-      if (ch.succeeded()) {
-        System.out.println("Connected to a server");
+        mqttClient.connect(BROKER_PORT, BROKER_HOST, ch -> {
+            if (ch.succeeded()) {
+                System.out.println("Connected to a server");
 
-        mqttClient.publish(
-          MQTT_TOPIC,
-          Buffer.buffer(MQTT_MESSAGE),
-          MqttQoS.AT_MOST_ONCE,
-          false,
-          false,
-          s -> mqttClient.disconnect(d -> System.out.println("Disconnected from server")));
-      } else {
-        System.out.println("Failed to connect to a server");
-        System.out.println(ch.cause());
-      }
-    });
-  }
+                mqttClient.publish(
+                        MQTT_TOPIC,
+                        Buffer.buffer(MQTT_MESSAGE),
+                        MqttQoS.AT_MOST_ONCE,
+                        false,
+                        false,
+                        s -> mqttClient.disconnect(d -> System.out.println("Disconnected from server")));
+            } else {
+                System.out.println("Failed to connect to a server");
+                System.out.println(ch.cause());
+            }
+        });
+    }
 
 }
--- src/main/java/tx/network/mqtt/test/Client.java
+++ src/main/java/tx/network/mqtt/test/Client.java
@@ -31,62 +31,63 @@ import java.nio.charset.Charset;
  */
 public class Client extends AbstractVerticle {
 
-  private static final String MQTT_TOPIC = "/my_topic";
-  private static final String MQTT_MESSAGE = "Hello Vert.x MQTT Client";
-  private static final String BROKER_HOST = "localhost";
-  private static final int BROKER_PORT = 1883;
+    private static final String MQTT_TOPIC = "/my_topic";
+    private static final String MQTT_MESSAGE = "Hello Vert.x MQTT Client";
+    private static final String BROKER_HOST = "localhost";
+    private static final int BROKER_PORT = 1883;
 
-  public static void main(String[] args) {
-    Launcher.executeCommand("run", Client.class.getName());
-  }
+    public static void main(String[] args) {
+        Launcher.executeCommand("run", Client.class.getName());
+    }
 
-  @Override
-  public void start() throws Exception {
-    MqttClientOptions options = new MqttClientOptions().setKeepAliveInterval(2);
+    @Override
+    public void start() throws Exception {
+        MqttClientOptions options = new MqttClientOptions().setKeepAliveInterval(2);
 
-    MqttClient client = MqttClient.create(Vertx.vertx(), options);
+        MqttClient client = MqttClient.create(Vertx.vertx(), options);
 
 
-    // handler will be called when we have a message in topic we subscribing for
-    client.publishHandler(publish -> {
-      System.out.println("Just received message on [" + publish.topicName() + "] payload [" + publish.payload().toString(Charset.defaultCharset()) + "] with QoS [" + publish.qosLevel() + "]");
-    });
+        // handler will be called when we have a message in topic we subscribing for
+        client.publishHandler(publish -> {
+            System.out.println("Just received message on [" + publish.topicName() + "] payload [" + publish.payload().toString(Charset.defaultCharset()) + "] with QoS [" + publish.qosLevel() + "]");
+        });
 
-    // handle response on subscribe request
-    client.subscribeCompletionHandler(h -> {
-      System.out.println("Receive SUBACK from server with granted QoS : " + h.grantedQoSLevels());
+        // handle response on subscribe request
+        client.subscribeCompletionHandler(h -> {
+            System.out.println("Receive SUBACK from server with granted QoS : " + h.grantedQoSLevels());
 
-      // let's publish a message to the subscribed topic
-      client.publish(
-        MQTT_TOPIC,
-        Buffer.buffer(MQTT_MESSAGE),
-        MqttQoS.AT_MOST_ONCE,
-        false,
-        false,
-        s -> System.out.println("Publish sent to a server"));
+            // let's publish a message to the subscribed topic
+            client.publish(
+                    MQTT_TOPIC,
+                    Buffer.buffer(MQTT_MESSAGE),
+                    MqttQoS.AT_MOST_ONCE,
+                    false,
+                    false,
+                    s -> System.out.println("Publish sent to a server"));
 
-      // unsubscribe from receiving messages for earlier subscribed topic
-      vertx.setTimer(5000, l -> client.unsubscribe(MQTT_TOPIC));
-    });
+            // unsubscribe from receiving messages for earlier subscribed topic
+            vertx.setTimer(5000, l -> client.unsubscribe(MQTT_TOPIC));
+        });
 
-    // handle response on unsubscribe request
-    client.unsubscribeCompletionHandler(h -> {
-      System.out.println("Receive UNSUBACK from server");
-      vertx.setTimer(5000, l ->
-        // disconnect for server
-        client.disconnect(d -> System.out.println("Disconnected form server"))
-      );
-    });
+        // handle response on unsubscribe request
+        client.unsubscribeCompletionHandler(h -> {
+            System.out.println("Receive UNSUBACK from server");
+            vertx.setTimer(5000, l ->
+                    // disconnect for server
+                    client.disconnect(d -> System.out.println("Disconnected form server"))
+            );
+        });
 
-    // connect to a server
-    client.connect(BROKER_PORT, BROKER_HOST, ch -> {
-      if (ch.succeeded()) {
-        System.out.println("Connected to a server");
-        client.subscribe(MQTT_TOPIC, 0);
-      } else {
-        System.out.println("Failed to connect to a server");
-        System.out.println(ch.cause());
-      }
-    });
-  }
+        // connect to a server
+        client.connect(BROKER_PORT, BROKER_HOST, ch -> {
+            if (ch.succeeded()) {
+                System.out.println("Connected to a server");
+                client.subscribe(MQTT_TOPIC, 0);
+            } else {
+                System.out.println("Failed to connect to a server");
+                System.out.println(ch.cause());
+            }
+        });
+
+    }
 }
